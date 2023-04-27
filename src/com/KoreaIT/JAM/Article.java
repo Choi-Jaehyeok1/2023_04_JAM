619c37982e47e1023d3e6ef3da14d27edbe32a1b
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-27 20:16:43 +0900

    MySQL connector J

--- src/com/KoreaIT/JAM/Article.java
+++ src/com/KoreaIT/JAM/Article.java
@@ -1,11 +1,11 @@
 package com.KoreaIT.JAM;
 
 public class Article {
-	
+
 	int id;
 	String title;
 	String body;
-	
+
 	public Article(int id, String title, String body) {
 		this.id = id;
 		this.title = title;
